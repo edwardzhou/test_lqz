8c7de5025115c774a1219fd47f3e07c47dbe4eb7
Edward Zhou <edward.zhq@gmail.com>
2018-02-11 00:01:17 +0800

    migration支持回滚

--- apps/auction/priv/repo/migrations/20180210153558_change_fields_to_text_for_authentication.exs
+++ apps/auction/priv/repo/migrations/20180210153558_change_fields_to_text_for_authentication.exs
@@ -1,9 +1,15 @@
 defmodule Auction.Repo.Migrations.ChangeFieldsToTextForAuthentication do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:authentications) do
       modify :token, :text
+    end
+  end
+
+  def down do
+    alter table(:authentications) do
+      modify :token, :string
     end
   end
 end
