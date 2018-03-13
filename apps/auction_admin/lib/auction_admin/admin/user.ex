4b1dfdb9b39b3e1af896d587709a83d76d33fd08
Edward Zhou <edward.zhq@gmail.com>
2018-03-13 20:57:28 +0800

    添加user翻译

--- apps/auction_admin/lib/auction_admin/admin/user.ex
+++ apps/auction_admin/lib/auction_admin/admin/user.ex
@@ -1,9 +1,21 @@
 defmodule AuctionAdmin.ExAdmin.User do
   use ExAdmin.Register
-
+  import AuctionAdmin.Gettext
   alias DB.Accounts.User
 
   register_resource User do
+    menu label: gettext("Users")
+
     scope :all, default: true
+
+    index do
+      column :id
+      column :username, label: gettext("User Name")
+      column :nickname, label: gettext("Nick Name")
+      column :telephone, label: gettext("Telephone")
+      column :email, label: gettext("Email")
+      actions()
+    end
+    
   end
 end
