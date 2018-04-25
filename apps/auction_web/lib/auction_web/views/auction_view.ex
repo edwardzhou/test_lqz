036363ef8fbea9dc40c329854d2031e30097d57b
breeze-yang <breeze@deshpro.com>
2018-04-25 18:45:34 +0800

    完成auction_web的首页

--- apps/auction_web/lib/auction_web/views/auction_view.ex
+++ apps/auction_web/lib/auction_web/views/auction_view.ex
@@ -1,11 +1,13 @@
 defmodule AuctionWeb.AuctionView do
   use AuctionWeb, :view
   alias Timex.Timezone
+  alias DB.Uploaders.Image
 
   def simple_datetime(time) do
-    local_time = Timezone.convert(time, "Asia/Shanghai")
-    {:ok, dt} = Timex.format(local_time, "{M}月{D}日 {h24}:{m}")
-    dt
+    Timex.local(time) |> Timex.format!("%Y-%m-%d %H:%M", :strftime)
   end
 
+  def img_url(logo) do
+    Image.url(logo)
+  end
 end
